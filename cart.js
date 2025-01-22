6a9470541eda26a3d3c598929c8612c3121acd8d
ivonnenabangala <ivonnenabangala@gmail.com>
2025-01-22 13:16:33 +0300

    display items in the cart

--- cart.js
+++ cart.js
@@ -1,7 +1,13 @@
 const cartApiUrl = "http://localhost:3000/cartItems"
+const productsApiUrl = "http://localhost:3000/products"
+let cartItems = document.querySelector(".cart-items")
+let cartId = localStorage.getItem("cartId") ? parseInt(localStorage.getItem("cartId")) : 1;
 
-async function addToCart( productId) {
-    // event.preventDefault()
+
+async function addToCart(productId, event) {
+    if (event) {
+        event.preventDefault(); 
+    }
     
     let loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
 
@@ -34,6 +40,7 @@ async function addToCart( productId) {
             })
         } else{
             const newCartItem = {
+                id: cartId,
                 userId,
                 products: [{productId, quantity:1}]
             }
@@ -45,6 +52,8 @@ async function addToCart( productId) {
                 },
                 body: JSON.stringify(newCartItem)
             })
+            cartId++;
+            localStorage.setItem("cartId", cartId);
         }
         console.log("Item added to cart successfully");
         
@@ -53,4 +62,110 @@ async function addToCart( productId) {
         console.log(error);
         
     }
+}
+async function getCartItems() {
+    let loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
+
+    if(!loggedUser){
+        alert("Login first to view cart")
+        return
+    }
+    let userId = loggedUser.id
+    let cartUrl = `${cartApiUrl}?userId=${userId}`
+    try {
+        const response = await fetch(cartUrl)
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const cartItems = await response.json();
+        console.log("Cart Items",cartItems);
+
+        if (!cartItems.length || !cartItems[0].products.length) {
+            console.log("No items in the cart.");
+            return;
+        }
+
+
+        let productIds = cartItems[0].products.map(item => item.productId);
+        console.log(productIds);
+
+        let productDetails = await Promise.all(
+            cartItems[0].products.map(async (cartItem) => {
+                let productUrl = `${productsApiUrl}/${cartItem.productId}`;
+                const productResponse = await fetch(productUrl);
+                
+                if (!productResponse.ok) {
+                    console.error(`Error fetching product ${cartItem.productId}: ${productResponse.status}`);
+                    return null;
+                }
+
+                let productData = await productResponse.json();
+                return { ...productData, quantity: cartItem.quantity }; 
+            })
+        )
+        console.log("Fetched products", productDetails);
+
+        
+
+        let cartContainer = document.getElementById("cartContainer");
+        
+        for (let product of productDetails) {
+            let totalPrice = product.price * product.quantity
+            console.log("Product", product);
+            
+            let cartCards = document.createElement('div')
+            cartCards.className = "cart-cards"
+
+            cartCards.innerHTML = `
+            <div class="cart-card">
+            <div class="product-image">
+
+                <img src="${product.image}" alt="product image">
+            </div>
+            <div class="content">
+                <h4>${product.productName}</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod dolore asperiores blanditiis nesciunt ab itaque in sapiente cumque deleniti dignissimos.</p>
+            </div>
+            <div class="pricing">
+                <p>Ksh. ${product.price}</p>
+                <i class="fa fa-minus" aria-hidden="true"></i>
+                <span>${product.quantity}</span>
+                <i class="fa fa-plus" aria-hidden="true"></i> <br>
+                <i class="fa fa-trash" aria-hidden="true" onclick="clearCart(${cartItems[0].products.productId})"></i>
+            </div>
+        </div>
+        <div class="checkout">
+            <h2>Total Price: ${totalPrice}</h2>
+            <button>Checkout</button>
+        </div>
+            `
+
+            cartContainer.appendChild(cartCards)
+        }
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+getCartItems()
+
+async function clearCart(id) {
+    const productUrl = `${cartApiUrl}/${id}`
+    console.log(productUrl)
+
+    let response = await fetch(productUrl, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+    }
+
 }
